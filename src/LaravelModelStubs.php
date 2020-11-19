b163cca26fb757621fd1d310256b72853ce54d56
mowangjuanzi <baoguoxiao0538@hotmail.com>
2020-11-19 10:25:00 +0800

    提交部分代码

---
+++ src/LaravelModelStubs.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Mowangjuanzi\LaravelModelStubs;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\LazyCollection;
+
+/**
+ * Trait LaravelModelStubs
+ * @method static Builder addBinding($value, $type = 'where')
+ * @method static Builder addNestedWhereQuery($query, $boolean = 'and')
+ * @method static Builder addSelect($column)
+ * @method static Builder addWhereExistsQuery(self $query, $boolean = 'and', $not = false)
+ * @method static mixed aggregate($function, $columns = ['*'])
+ * @method static Builder applyScopes()
+ * @method static mixed average($column)
+ * @method static Model|Builder create(array $attributes = [])
+ * @method static LazyCollection cursor()
+ * @method static int decrement($column, $amount = 1, array $extra = [])
+ * @method static mixed delete()
+ * @method static array eagerLoadRelations(array $models)
+ * @method static Model|Collection|Builder[]|null find($id, $columns = ['*'])
+ * @method static Collection findMany($ids, $columns = ['*'])
+ * @method static Model|Collection|Builder|Builder[] findOrFail($id, $columns = ['*'])
+ * @method static Model|Builder findOrNew($id, $columns = ['*'])
+ */
+trait LaravelModelStubs
+{
+
+}
